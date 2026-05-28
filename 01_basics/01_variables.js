7768475b3e7a35e3d1c8053f550c4f0fd9895dd8
Priyanka Dighwa <priyanka1bdighwa@gmail.com>
2026-05-28 17:54:42 +0530

    basicVariable

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 1441
+let accountEmail = "sjhjkd@gmail.com"
+var accountPassword = "28242834hsfrh"
+
+accountEmail = "hc@gmail.com"
+accountPassword = "ahduhc123"
+let acoountState ;
+// accountId = 2; ---> Not allowed
+
+// do not use var because of issue in block scope and functional scope
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,acoountState]);
